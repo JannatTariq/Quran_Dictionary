c0f831f4d96a38dcad5e82e37dd3010ee0eeac56
JannatTariq <jannattariq08@gmail.com>
2026-06-20 22:59:51 +0500

    restructure project

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -1,16 +1,18 @@
 import dbConnect from "@/lib/db";
-import { getCategories } from "@/lib/category";
+import Category from "@/models/category";
 
 export async function GET() {
   try {
     await dbConnect();
 
-    const categories = await getCategories();
+    const categories = await Category.find({});
 
-    return Response.json({ categories });
+    return Response.json({
+      categories: Array.isArray(categories) ? categories : [],
+    });
   } catch (err) {
-    console.error(err);
+    console.error("CATEGORY API ERROR:", err);
 
-    return Response.json({ message: "Something went wrong" }, { status: 500 });
+    return Response.json({ error: err.message }, { status: 500 });
   }
 }
